ee47be21dd981164893e85ad2062d961e7f2e9ac
Md Shad Ahmad <136076679+mdshadahmad160@users.noreply.github.com>
2024-10-14 21:14:01 +0530

    initial commit

---
+++ src/test/java/com/shad/EnotesApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnotesApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
